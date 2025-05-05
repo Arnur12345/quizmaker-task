c3bf112b803d542ddd5b757af68bf4d57c3e00ea
Artyqbay Arnur <113910571+Arnur12345@users.noreply.github.com>
2025-05-05 15:31:00 +0500

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,7 @@ def create_app():
     app.config.from_object(Config)
     
     # Настраиваем CORS для всего приложения
-    CORS(app, resources={r"/api/*": {"origins": ["http://localhost:3000", "http://127.0.0.1:3000"]}})
+    CORS(app, resources={r"/api/*": {"origins": ["http://localhost:3000", "http://127.0.0.1:3000", "https://quizmaker-nfac.vercel.app"]}})
 
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
     app.register_blueprint(quiz_bp, url_prefix='/api/quiz')
