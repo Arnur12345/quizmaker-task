ec8c2748010534fee6606fa840a65a8e4afef0c4
Artyqbay Arnur <113910571+Arnur12345@users.noreply.github.com>
2025-05-05 20:29:55 +0500

    Update quiz.py

--- backend/quiz/quiz.py
+++ backend/quiz/quiz.py
@@ -368,7 +368,7 @@ def generate_quiz(current_user):
         # Используем Groq для генерации викторины
         
         client = Groq(
-            api_key=os.environ.get("GROQ_API_KEY")
+            api_key=os.environ.get("GROQ_API_KEY", "")
         )
         chat_completion = client.chat.completions.create(
             model="llama3-70b-8192",
