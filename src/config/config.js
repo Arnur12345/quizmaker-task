8787f778291169ce5479c59f97d3e162fed05db0
Artyqbay Arnur <113910571+Arnur12345@users.noreply.github.com>
2025-05-05 14:44:40 +0500

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
     apiBaseUrl:
       process.env.NODE_ENV === "production"
-        ? "https://quizmaker-task.onrender.com"
+        ? "https://quizmaker-task.onrender.com/api"
         : "http://127.0.0.1:5000/api",
   };
   
