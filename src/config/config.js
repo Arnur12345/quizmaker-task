942f85b72102109eaf7943626b540a942b9ffef2
Arnur12345 <dominusvf@gmail.com>
2025-05-05 14:15:24 +0500

    fix

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 const config = {
     apiBaseUrl:
       process.env.NODE_ENV === "production"
-        ? "https://your-production-api.com"
+        ? "https://quizmaker-task.onrender.com"
         : "http://127.0.0.1:5000/api",
   };
   
